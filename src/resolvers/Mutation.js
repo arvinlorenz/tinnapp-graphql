fe5cf7edba4a67becca2366de8ab8e3f8449f158
arvin <arvinlorenz@gmail.com>
2019-05-31 23:34:35 +0800

    initial

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -62,6 +62,194 @@ const Mutation = {
             },
             data: args.data
         }, info)
+    },
+
+    async createOrder(parent, args, { prisma, request }, info){
+        const userId = getUserId(request)
+        //total price, shippingFee, orderDate will be coming from the front end
+
+        let opArgs = {}
+        opArgs.data = {
+            ...args.data
+        }
+        opArgs.data.createdBy = {
+            connect:{
+                id: userId
+            }
+        }
+        opArgs.data.buyer = {
+            connect:{
+                id: args.data.buyer
+            }
+        }
+        opArgs.data.products = {
+            connect: args.data.products.map(id=>{
+                return { id }
+            })
+        }   
+
+        return prisma.mutation.createOrder(opArgs,info)
+
+    },
+    async deleteOrder(parent, args, { prisma, request }, info){
+        const userId = getUserId(request)
+
+        const user = await prisma.query.user(
+            {
+             where:{
+                 id:userId
+             }
+            } 
+         )
+        const accountType = user.accountType
+        
+        const exists = await prisma.exists.Order({
+            id: args.id,
+            createdBy: {
+                id: userId
+            }
+        })
+        if(!exists && accountType !== "PROVINCIAL_DISTRIBUTOR"){
+            throw new Error("Not allowed to delete order")
+        }
+   
+
+        return prisma.mutation.deleteOrder({
+            where:{
+                id: args.id
+            }
+        })
+    },
+
+    async updateOrder(parent, args, { prisma, request }, info){
+        const userId = getUserId(request)
+
+        const user = await prisma.query.user(
+            {
+             where:{
+                 id:userId
+             }
+            } 
+         )
+        const accountType = user.accountType
+        
+        const exists = await prisma.exists.Order({
+            id: args.id,
+            createdBy: {
+                id: userId
+            }
+        })
+        if(!exists && accountType !== "PROVINCIAL_DISTRIBUTOR"){
+            throw new Error("Not allowed to delete order")
+        }
+
+        let opArgs ={}
+
+        opArgs.data = {
+            ...args.data
+        }
+        opArgs.data.products = {
+            set: args.data.products.map(id=>{
+                return { id }
+            })
+        }   
+
+        console.log(JSON.stringify(opArgs, undefined, 3))
+        return prisma.mutation.updateOrder({
+            ...opArgs,
+            where:{
+                id: args.id
+            }
+        }, info)
+    },
+
+    async processOrder(parent, args, { prisma, request }, info){
+        const userId = getUserId(request)
+
+        const user = await prisma.query.user(
+            {
+             where:{
+                 id:userId
+             }
+            } 
+         )
+        const accountType = user.accountType
+
+        if(accountType !== "PROVINCIAL_DISTRIBUTOR"){
+            throw new Error("Not allowed to process order")
+        }
+
+        return prisma.mutation.updateOrder({
+            data:{
+                purchaseDate: args.purchaseDate,
+                isPaid: true
+            },
+    
+            where:{
+                id: args.id
+            }
+        }, info)
+    },
+
+    async createProduct(parent, args, { prisma, request }, info){
+        const userId = getUserId(request)
+
+        const user = await prisma.query.user(
+            {
+             where:{
+                 id:userId
+             }
+            } 
+         )
+        const accountType = user.accountType
+
+        if(accountType !== "PROVINCIAL_DISTRIBUTOR"){
+            throw new Error("Not allowed to create product")
+        }
+        //create price lists
+        let opArgsPrice = {}
+        opArgsPrice.data = {
+            ...args.data.price
+        }
+        
+        let price = await prisma.mutation.createPrice(opArgsPrice, info)
+
+        // create product
+        let opArgsProduct = {}
+        opArgsProduct.data = {
+            ...args.data
+        }
+        opArgsProduct.data.price = {
+            connect:{
+                id: price.id
+            }
+        }
+
+      
+        return prisma.mutation.createProduct(opArgsProduct, info)
+    },
+
+    async deleteProduct(parent, args, {prisma, request}, info){
+        const userId = getUserId(request)
+
+        const user = await prisma.query.user(
+            {
+             where:{
+                 id:userId
+             }
+            } 
+         )
+        const accountType = user.accountType
+        
+        if(accountType !== "PROVINCIAL_DISTRIBUTOR"){
+            throw new Error("Not allowed to create product")
+        }
+
+        return prisma.mutation.deleteProduct({
+            where:{
+                id: args.id
+            }
+        })
     }
 }
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -27,6 +27,43 @@ const Query = {
                 id: userId
             }
         })
+    },
+
+    orders(parent, args, { prisma }, info) {
+        let opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        }
+        
+        if (typeof(args.isPaid) === "boolean") {
+            opArgs.where = {
+                isPaid: args.isPaid
+            }
+        }
+        return prisma.query.orders(opArgs, info)
+    },
+
+    products(parent, args, { prisma }, info){
+        let opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        }
+        
+        if (args.query) {
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                },
+                {
+                    code_contains: args.query
+                }]
+            }
+        }
+        return prisma.query.products(opArgs, info)
     }
 }
 
