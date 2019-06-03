3b6e2f557d913531c1912098cbec64e256e9c133
arvin <arvinlorenz@gmail.com>
2019-06-03 14:06:12 +0800

    added angular

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'orders',
+    loadChildren: './orders/orders.module#OrdersPageModule'
+  },
+  { path: 'orders', loadChildren: './orders/orders.module#OrdersPageModule' }
+ 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
