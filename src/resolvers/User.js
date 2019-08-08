2de2b5c34cbf257c37550f2f692cc53257d9969d
arvin lorenz <arvinlorenz@gmail.com>
2019-08-08 17:35:42 +0800

    users

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -3,10 +3,17 @@ import getUserId from '../utils/getUserId'
 const User = {
     email: {
         fragment: 'fragment userId on User { id }',
-        resolve(parent, args, { request }, info) {
+        async resolve(parent, args, { request, prisma }, info) {
             const userId = getUserId(request, false)
-
-            if (userId && userId === parent.id) {
+            const user =  await prisma.query.user(
+                {
+                 where:{
+                     id:userId
+                 }
+                } 
+             )
+            const accountType = user.accountType;
+            if ((userId && (userId === parent.id)) || accountType === 'PROVINCIAL_DISTRIBUTOR') {
                 return parent.email
             } else {
                 return null
